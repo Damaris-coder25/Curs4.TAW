214d59548fba8194e3c87bbc7bae507d94d78249
Damaris <damaris.ionel@emanuel.ro>
2025-11-21 11:55:04 +0200

    Insert page computed

---
+++ src/computed/FullName.vue
@@ -0,0 +1,40 @@
+<script setup>
+import {ref} from 'vue'
+import { computed } from 'vue'
+const emit = defineEmits(['update:fullName'])
+
+const firstName=ref("")
+const lastName=ref("")
+
+const fullName= computed(() => {
+    const fN = firstName.value + " " + lastName.value
+    emit("update:fullName", fN)
+    return fN
+})
+
+const greeting = computed(() => "Hello," + fullName.value + "!")
+
+const showFullName = ref(false);
+</script>
+
+<template>
+    First Name:
+    <input class="border" type="text" v-model="firstName">
+    <br>
+    Last Name:
+    <input class="border" type="text" v-model="lastName">
+    <br>
+    <br>
+    <button 
+    class="border border-gray-400 px-2" 
+    type="button" 
+    @click="showFullName= !showFullName">
+        <span v-if="showFullName">Hide</span>
+        <span v-else>Show</span> full name
+    </button>
+
+    <div v-if="showFullName">
+    <p>Your Full Name: {{ fullName}}</p>
+    <p>{{ greeting }}</p>
+    </div>
+</template>
---
+++ src/pages/Computed.vue
@@ -0,0 +1,30 @@
+<script setup>
+import FullName from '@/computed/FullName.vue';
+import PriceCalculator from '@/computed/PriceCalculator.vue';
+import SupplierList from '@/computed/SupplierList.vue';
+import { ref } from 'vue';
+const fullName = ref("");
+const addToList = () => {
+    
+};
+</script>
+
+<template>
+    <h2 class="text-xl font-bold">Computed Page</h2>
+    <FullName @update:fullName="fN => fullName = fN" />
+        <p>Your full name from parent component: {{ fullName }}</p>
+    <hr>
+    <PriceCalculator />
+    <hr>
+    <button 
+    class="border border-gray-400 px-2 my-4" @click="addToList">
+    Add to List
+    </button>
+    <hr>
+    <SupplierList />
+</template>
+
+<style>
+@import "bootstrap-icons";
+@import "tailwindcss";
+</style>
